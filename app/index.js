ad0eef017ce35a53f5e931c121d5f2ba34e4e367
Marco Staccato <marcostaccato@gmail.com>
2020-02-23 18:25:08 -0600

    feat: add navbar component

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 //component -> state, lifecycle, UI
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='container'>
+        <Popular />
       </div>
     ) 
   }
